b84b6c9997342753a852eed499aa3d84804fe52d
sora-tt <sora.tomita@togawa.cs.waseda.ac.jp>
2025-09-21 11:18:53 +0900

    initial commit for both frontend and backend

---
+++ backend/no_cover_vocabulary/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for no_cover_vocabulary project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'no_cover_vocabulary.settings')
+
+application = get_wsgi_application()
